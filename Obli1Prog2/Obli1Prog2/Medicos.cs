3e6aa92607ede7b4bb662329f9b972a26a22f51f
Benjamín López <benjaminlh2406@gmail.com>
2025-10-21 16:55:56 -0300

    Añadí una carpeta nueva con la solucion correcta y el codigo nuevo

---
+++ Obli1Prog2/Obli1Prog2/Medicos.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obli1Prog2
+{
+    internal class Medicos
+    {
+        private static int ContadorID = 1;
+
+        #region Propiedades
+        public int IdMedico { get; private set; }
+        public string NombreM { get; set; }
+        public string ApellidoM { get; set; }
+        public string Especialidad { get; set; }
+        public string Matricula { get; set; }
+        public string NombreUsuarioM { get; set; }
+        public string ContraseniaM { get; set; }
+        public string DiasAtencion { get; set; }
+        public int[] HorariosDisponibles { get; set; } //Tipo de dato a revisar.
+        #endregion
+
+        #region Constructores
+        public Medicos(string nombre, string apellido, string especialidad, string matricula,
+            string nombreUsuario, string contrasenia, string diasAtencion, int[] horariosDisponibles)
+        {
+            IdMedico = ContadorID++;
+            NombreM = nombre;
+            ApellidoM = apellido;
+            Especialidad = especialidad;
+            Matricula = matricula;
+            NombreUsuarioM = nombreUsuario;
+            ContraseniaM = contrasenia;
+            DiasAtencion = diasAtencion;
+            HorariosDisponibles = horariosDisponibles;
+        }
+        #endregion
+
+        #region Metodos
+        public override string ToString()
+        {
+            return $"Médico: ID: {IdMedico}, Nombre: {NombreM}, Apellido{ApellidoM}, Especialidad: {Especialidad}, Matricula: {Matricula}, Dias de atención: {DiasAtencion}, Horarios disponible: {HorariosDisponibles}";
+        }
+        #endregion
+    }
+}
---
+++ Obli1Prog2/Obli1Prog2/Pagos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obli1Prog2
+{
+    internal class Pagos
+    {
+        private static int ContadorID = 1;
+
+        #region Propiedades
+        public int IdPago { get; private set; }
+        public Turnos IdTurno { get; private set; }
+        public DateOnly FechaPago { get; set; } //Tipo de dato a revisar.
+        public int Monto { get; set; }
+        public string MetodoPago { get; set; }
+        #endregion
+
+        #region Constructores
+        public Pagos(Turnos idTurno, DateOnly fechaPago, int monto, string metodoPago)
+        {
+            IdPago = ContadorID++;
+            IdTurno = idTurno;
+            FechaPago = fechaPago;
+            Monto = monto;
+            MetodoPago = metodoPago;
+        }
+        #endregion
+
+        #region Metodos
+        public override string ToString()
+        {
+            return $"Pago: ID Pago: {IdPago}, ID Turno: {IdTurno.IdTurno}, Fecha de pago: {FechaPago}, Monto: {Monto}, Metodo de pago: {MetodoPago}";
+        }
+        #endregion
+    }
+}
